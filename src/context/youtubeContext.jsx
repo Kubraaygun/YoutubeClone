5f173402be091bf78b45e02bf345f75145e256cd
kubraaygun <kubraaygun@outlook.com.tr>
2024-01-16 18:40:29 +0300

    gif eklendi ve duzenleme yapildi

--- src/context/youtubeContext.jsx
+++ src/context/youtubeContext.jsx
@@ -1,38 +1,23 @@
-import { createContext, useEffect, useState } from 'react';
-import { categories } from '../constants';
-import { getData } from '../utils/getData';
+import { createContext, useEffect, useState } from "react";
+import { categories } from "../constants";
+import { getData } from "../utils/getData";
 
-// 1) context temelini oluştur
 export const YoutubeContext = createContext();
-
-// 2) context'de tutulan verileri uygulamaya aktarıcak
 export const YoutubeProvider = ({ children }) => {
-  const [selectedCategory, setSelectedCategory] = useState(
-    categories[0]
-  );
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   const [videos, setVideos] = useState(null);
 
-  // selectedCategory her değiştiğinde api'den
-  // ilgili  kategorinin veirlerini çek
   useEffect(() => {
-    // farklı veirleri alırken öncekileri temizle
     setVideos(null);
-
-    // eğerki seçili kategorini tipi home veya trending
-    // ise o kategorini tipinin ismine istek at
     if (
-      selectedCategory.type === 'home' ||
-      selectedCategory.type === 'trending'
+      selectedCategory.type === "home" ||
+      selectedCategory.type === "trending"
     ) {
-      getData(`/${selectedCategory.type}`).then((data) =>
-        setVideos(data.data)
-      );
+      getData(`/${selectedCategory.type}`).then((data) => setVideos(data.data));
     }
 
-    // eğeki seçili kategornin tipi category ise
-    // o zaman search endpointine istek at
-    if (selectedCategory.type === 'category') {
-      getData(`/search?query=${selectedCategory.name}`).then((data) =>
+    if (selectedCategory.type === "category") {
+      getData(`/search?query= ${selectedCategory.name}`).then((data) =>
         setVideos(data.data)
       );
     }
