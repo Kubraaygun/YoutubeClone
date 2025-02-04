21aa9e443b4deb42810e001e38dc0f718d13c27d
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-02-04 21:31:04 +0300

    update

---
+++ .history/src/utils/getData_20250204213050.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://yt-api.p.rapidapi.com";
+
+const options = {
+  params: { geo: "TR", lang: "tr" },
+
+  headers: {
+    "X-RapidAPI-Key": REACT_APP_API_KEY,
+    "X-RapidAPI-Host": "yt-api.p.rapidapi.com",
+  },
+};
+
+export const getData = async (path) => {
+  try {
+    const response = await axios.get(path, options);
+    //console.log(response);
+    return response.data;
+  } catch (err) {
+    console.error("error data", err);
+  }
+};
